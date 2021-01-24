ec96b472ba6ad4aae179f77a751116d489b8087f
Suyashtnt <suyashtnt@gmail.com>
2021-01-24 13:16:24 +0200

    feat(i18n): i18n-fy auth modal

--- src/renderer/locales/en.js
+++ src/renderer/locales/en.js
@@ -8,5 +8,12 @@ module.exports = {
       subtitle: 'Go ahead, add another account or your first one!',
       placeholder: 'Accounts'
     }
+  },
+  authModal: {
+    title: 'Add user',
+    email: 'email',
+    password: 'password',
+    submit: 'submit',
+    close: 'close'
   }
 }
